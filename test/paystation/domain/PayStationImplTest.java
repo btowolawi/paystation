eaf0d8756e0ee19da33f19106d6558165eb796bd
Busola Towolawi <bvtowolawi@gmail.com>
2018-11-28 03:44:31 -0500

    Call to empty returns the total amount entered: failed

--- test/paystation/domain/PayStationImplTest.java
+++ test/paystation/domain/PayStationImplTest.java
@@ -1,13 +1,5 @@
 /**
  * Testcases for the Pay Station system.
- *
- * This source code is from the book "Flexible, Reliable Software: Using
- * Patterns and Agile Development" published 2010 by CRC Press. Author: Henrik B
- * Christensen Computer Science Department Aarhus University
- *
- * This source code is provided WITHOUT ANY WARRANTY either expressed or
- * implied. You may study, use, modify, and distribute it for non-commercial
- * purposes. For any commercial use, see http://www.baerbak.com/
  */
 package paystation.domain;
 
@@ -137,5 +129,17 @@ public class PayStationImplTest {
         ps.addPayment(25);
         assertEquals("Insert after cancel should work",
                 10, ps.readDisplay());
+    }
+    
+    /**
+     * Call to empty returns the total amount entered.
+     */
+    @Test
+    public void shouldReturnTotalAmountAfterEmpty()
+            throws IllegalCoinException{
+        ps.addPayment(25);
+        
+        ps.empty();
+        assertEquals(25, ps.empty());
     }
 }
